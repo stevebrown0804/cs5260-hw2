466219013717d77a26f6aa86dfdf894a00c76e0f
stevebrown0804 <stevebrown0804@gmail.com>
2022-10-21 14:23:36 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import argparse
+
+# cmd-line arguments
+print(argparse.__version__)
+parser = argparse.ArgumentParser()
+parser.parse_args()
+
+
+
+# check for presence of a widget
+#  if it's there, read it, delete it and add it to <wherever>
+
+
+# log the transaction
