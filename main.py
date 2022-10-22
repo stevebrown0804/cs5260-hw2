57859eb427e5898ffc153352996f7e4921a4cafe
stevebrown0804 <stevebrown0804@gmail.com>
2022-10-22 16:12:09 -0600

    adding dynamoDB writing

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
+import os
 import argparse  # https://docs.python.org/3/howto/argparse.html#id1
-import boto3 as boto3  # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Object.delete
+import boto3 as boto3
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Object.delete
 # https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/python/example_code/s3/s3_basics/object_wrapper.py
 from botocore.exceptions import ClientError
 import logging  # https://docs.python.org/3/howto/logging.html#when-to-use-logging
 import time
 import json
-import unittest   # https://docs.python.org/3/library/unittest.html
+import unittest  # https://docs.python.org/3/library/unittest.html
 
 # Reminder: To update the credentials for AWS, get them from the learner lab module (under AWS details->AWS CLI)
 # and paste them into C:\Users\steve\.aws\credentials
 
+# When a Widget needs to be stored in Bucket 3, you should serialize it into a JSON string and store that string
+# data. Its key should be based on the following pattern:
+# widgets/{owner}/{widget id}
+# where {owner} is derived from the widget’s owner and {widget id} is derived from the widget’s
+# id. The {owner} part of the key should be computed from the Owner property by 1) replacing
+# spaces with dashes and converting the whole string to lower case.
+#
+# When a widget needs to be stored in the DynamoTable, place every widget attribute in the
+# request its own attribute in the DynamoDB object. In other words, in addition to the widget_id,
+# owner, label, and description, all the properties listed in the otherAttributes properties need to
+# be stored as attributes in the DynamoDB object and not as single map or list.
+
 # Parse the command-line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--read-from", help="name of the bucket to read from, eg. usu-cs5260-cocona-requests or bucket2")
-parser.add_argument("--write-to", help="target to write to, eg. usu-cs5260-cocona-web or bucket3 or widgets_table")
+parser.add_argument("--write-to", help="target to write to, eg. usu-cs5260-cocona-web or bucket3 or dynamoDB")
 args = parser.parse_args()
 if args.read_from:
     read_from = args.read_from
@@ -54,8 +68,20 @@ def delete(self):
         raise
 
 
+def insert_into_dynamodb(to_write, key_name):
+    try:
+        dynamo_table = dynamodb.Table("widgets")
+        dynamo_table.put_item(Item=to_write)
+        logger.info("Put item %s into dynamoDB", to_write)
+    except ClientError:
+        logger.exception("Couldn't insert item %s into dynamoDB", to_write)
+        raise
+
+
 # Create the bucket(s)
 s3 = boto3.resource('s3')
+client = boto3.client("s3")
+dynamodb = boto3.resource('dynamodb')
 bucket2 = s3.Bucket(f'usu-cs5260-cocona-requests')
 if write_to == "bucket3" or "usu-cs5260-cocona-web":
     bucket3 = s3.Bucket(f'usu-cs5260-cocona-web')
@@ -71,10 +97,30 @@ while not done:
         # bucket2.download_file(the_object, 'the_object')
         bucket2.download_file(the_object, the_object)
         s3.Object(bucket2, objs[0]).key.delete()
-        # ...and write it to the write-to target TODO
-        #   create, update and delete widgets
-        #   store the widget(s) in bucket 3 or in the dynamoDB 'widgets' table
+        # ...and write it to the write-to target
+        #   LATER: create, update and delete widgets
+        #           store the widget(s) in bucket 3 or in the dynamoDB 'widgets' table
         #
+        if write_to == "usu-cs5260-cocona-web":
+            client.upload_file(the_object, write_to, "widgets/" + the_object)
+        elif write_to == "dynamoDB":
+            # print("TODO: Insert the file into dynamoDB")                                               -- IN PROGRESS
+            with open(the_object, 'r') as a_file:
+                data = a_file.read()
+            the_data = json.loads(data)
+            print("JSON data: ")
+            print(the_data)
+            insert_into_dynamodb(the_data, the_object)
+        else:
+            logger.error("Unrecognized write-to target")
+            print("Unrecognized write-to target")
+            quit()
+        # ...then delete the local copy
+        if os.path.exists(the_object):
+            os.remove(the_object)
+            logger.info("The file: %s has been deleted", the_object)
+        else:
+            logger.info("The file: %s does not exist", the_object)
     else:
         # if it's not, wait 100ms and try again
         time.sleep(0.1)
